75cdd70a83115946aa27d05b55b100a440046d77
devert007 <kislydevert007@gmail.com>
2025-10-25 11:18:57 +0300

    add pydantic_models for auth

--- api/pydantic_models.py
+++ api/pydantic_models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field,EmailStr
 from enum import Enum
 from datetime import datetime
 from typing import Optional
@@ -32,3 +32,29 @@ class TestPDFInfo(BaseModel):
 
 class DeleteFileRequest(BaseModel):
     file_id: int
+
+
+class UserCreate(Basemodel):
+    username: str = Field(..., min_length=2, max_length=50)
+    email:EmailStr
+    password:str = Field(...,min_length=6)
+
+class UserLogin(BaseModel):
+    username:str
+    password:str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    is_active: bool
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    user: UserResponse
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
